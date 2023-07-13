41083e4df09c7062d9b54ce023cf1e51dce7d220
Luigi <128727133+luigibuono@users.noreply.github.com>
2023-07-13 12:30:43 +0200

    Update java.js

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,87 +1,23 @@
-class Pulsanti {
-  constructor(elem) {
-    this._elem = elem;
-    elem.onclick = this.onClick.bind(this); // (*)
-  }
+let add = document.getElementById ('incrementa');
+let remove = documenti.getElementById('decrementa');
+let reset = document.getElementById('resetta');
+let numero = document.getElementById('counter')
 
-  /* Evento bottone azzera */
-  azzera() {
-      let cont = document.getElementsByTagName("p")[0].innerHTML;
-      cont = Number(cont);
-      cont = 0;
-      cont = cont.toString();
-      document.getElementsByTagName("p")[0].innerHTML = cont; 
-  }
+let numero = 0;
 
-  /* Evento bottone incremento */
-  incremento() {
-      let cont = document.getElementsByTagName("p")[0].innerHTML;
-      cont = Number(cont);
-      let num = document.getElementById("incremento").value;
-      num = Number(num);  
-      if (num <= 0) {
-          alert("Non posso incrementare per 0 o per valore negativo");
-      } else {
-          cont += num;
-      }
-      cont = cont.toString();
-      if (cont.length > 9) {
-          alert("Il contatore è arrivato al limite!");
-      } else {
-          document.getElementsByTagName("p")[0].innerHTML = cont; 
-      }
-  }
+add.addEventListener('click',function(){
 
-  /* Evento bottone decremento */
-  decremento() {
-      let cont = document.getElementsByTagName("p")[0].innerHTML;
-      cont = Number(cont);
-      let num = document.getElementById("incremento").value;
-      num = Number(num);  
-      if (num <= 0) {
-          alert("Non posso decrementare per 0 o per valore negativo");
-      } else {
-          cont -= num;
-      }
-      cont = cont.toString();
-      if (cont.length > 9) {
-          alert("Il contatore è arrivato al limite!");
-      } else {
-          document.getElementsByTagName("p")[0].innerHTML = cont; 
-      } 
-  }
+                     numero +=1;
+                     int.innerHTML = numero;});
 
-  onClick(event) {
-      let action = event.target.dataset.action;
-      if (action) {
-        this[action]();
-      }
-    };
-}
-
-new Pulsanti(buttons);
+remove.addEventListener('click', function(){
+    numero -=1;
+    int.innerHTML=numero;
+})
 
 
-let divCont = document.getElementById("display-contatore");
-let divAzzera = document.getElementById("button-azzera");
-let divButtons = document.getElementById("buttons-inc");
-
-/* Implementazioni contatore e bottoni */
-let contatore = document.createElement("p");               
-contatore.innerText = "0";             
-divCont.appendChild(contatore); 
-
-let buttonAzzera = document.createElement("button");
-buttonAzzera.innerHTML = "Azzera";
-buttonAzzera.setAttribute("data-action","azzera"); 
-divAzzera.appendChild(buttonAzzera);
-        
-let button = document.createElement("button");
-button.innerHTML = "+";
-button.setAttribute("data-action","incremento"); 
-divButtons.appendChild(button); 
-
-let button2 = document.createElement("button");
-button2.innerHTML = "-"; 
-button2.setAttribute("data-action","decremento"); 
-divButtons.appendChild(button2); 
+reset.addEventListener('click', function(){
+    numero = 0;
+    int.innerHTML = numero;
+})
+                     
